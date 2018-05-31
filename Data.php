534ffd3a192c0da9b1868a0716fdc31a9ff9c4e1
openSIS Administrator <info@os4ed.com>
2018-05-31 02:06:49 -0400

    Delete Data.php

--- Data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$DatabaseType = 'mysqli'; 
-$DatabaseServer = 'localhost'; 
-$DatabaseUsername = 'root'; 
-$DatabasePassword = ''; 
-$DatabaseName = 'responsive'; 
-$DatabasePort = '3306'; 
-?>
