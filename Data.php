4f432c27cce456c3aad285fca5329614acf16ad9
OS4ED Administrator <bob@os4ed.com>
2020-01-16 14:52:43 -0500

    Delete data.php
    Deleet the data.php file. That went up by mistake.

--- Data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$DatabaseType = 'mysqli'; 
-$DatabaseServer = 'localhost'; 
-$DatabaseUsername = 'root'; 
-$DatabasePassword = 'Ge0rg1a30097%go'; 
-$DatabaseName = 'bobtest'; 
-$DatabasePort = '3306'; 
-?>
