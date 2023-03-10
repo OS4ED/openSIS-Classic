61a2f2bdc9be1da17c4c0f76d168a804940f8f05
Sayan Mukherjee <developer1@os4ed.com>
2023-03-10 21:15:41 +0530

    Commit regarding fixing of the vulnerability issues
    ### Commit includes:
    
    - Authenticated Reflected Cross Site Scripting (XSS) in the page - `ForExport.php`.
    - Unauthenticated Reflected Cross Site Scripting (XSS) in the page - `CheckUrlWs.php`.

--- CheckUrlWs.php
+++ CheckUrlWs.php
@@ -1,15 +1,25 @@
 <?php
-$url = $_REQUEST['url'];
+
+include 'functions/SqlSecurityFnc.php';
+
+$requestURL = sqlSecurityFilter($_REQUEST['url'], 'no');
+
+if (!defined("_validUrl"))
+    define("_validUrl", "valid url");
+if (!defined("_invalidUrl"))
+    define("_invalidUrl", "invalid url");
+
+$url = $requestURL;
 $data = array();
 $exists = '';
 $file_headers = '';
-$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+$s = (empty($_SERVER["HTTPS"]) ? '' : (($_SERVER["HTTPS"] == "on") ? "s" : ""));
 $sp = strtolower($_SERVER["SERVER_PROTOCOL"]);
 $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
 $out=$protocol . "://" . $url;
 $file_headers = @get_headers($out);
 
-if(count($file_headers)>1)
+if(is_countable($file_headers) && count($file_headers)>1)
 {
     if($file_headers[0]!='' && strpos($file_headers[0], '404')) {
         $exists = 0;
